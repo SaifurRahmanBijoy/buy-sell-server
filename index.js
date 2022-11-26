ca7d0e21f553d25a054ffacb644b550b5b12fa51
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-11-26 18:11:04 +0600

    done seller and buyer deletion api

--- index.js
+++ index.js
@@ -71,6 +71,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const specifiedUsers = { _id: ObjectId(id) };
+      const result = await usersCollection.deleteOne(specifiedUsers);
+      res.send(result);
+    });
+
     app.get("/users/admin/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email };
