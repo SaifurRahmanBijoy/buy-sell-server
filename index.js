995468a2ef09bb9b1449d52e1a1508bcf19f07bd
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-11-27 15:47:46 +0600

    done api for products deletion

--- index.js
+++ index.js
@@ -84,6 +84,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/deleteproduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const product = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(product);
+      res.send(result);
+    });
+
     app.post("/myproducts/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: ObjectId(id) };
